e2a5d1cb987857d9ebd8b21215879394c936b4f0
nvanmiddendorp <nvanmiddendorp@yahoo.com>
2013-10-13 18:05:01 -0500

    Create MenuItem.java

---
+++ MenuItem.java
@@ -0,0 +1,50 @@
+package restauranttester;
+
+public class MenuItem
+{
+    private String itemCode;
+    private String itemName;
+    private double itemPrice;
+ 
+    public MenuItem(String itemCode, String itemName, double itemPrice)
+    {  
+        this.itemCode = itemCode;
+        this.itemName = itemName;
+        this.itemPrice = itemPrice;
+    }
+    
+    public MenuItem()
+    {
+        
+    }
+    
+    public void setItemCode(String itemCode)
+    {
+        this.itemCode = itemCode;
+    }
+    
+    public void setItemName(String itemName)
+    {
+        this.itemName = itemName;
+    }
+    
+    public void setItemPrice(double itemPrice)
+    {
+         this.itemPrice = itemPrice;
+    }
+    
+    public String getItemCode()
+    {
+        return itemCode;
+    }
+    
+    public String getItemName()
+    {
+        return itemName;
+    }
+    
+    public double getItemPrice()
+    {
+        return itemPrice;
+    }    
+}
