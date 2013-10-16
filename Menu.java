bbac5dfd24c383fd7eb41e1b6724f7860e27cb3f
nvanmiddendorp <nvanmiddendorp@yahoo.com>
2013-10-16 05:08:17 -0500

    Update Menu.java
    Updating 10/16/13.  This now does nothing, but will be fixed later.

--- Menu.java
+++ Menu.java
@@ -3,18 +3,19 @@ package restauranttester;
 import java.util.ArrayList;
 
 public class Menu
-{  
+{   
+    public ArrayList<MenuItem> menuList = new ArrayList<>();
+    
     public Menu()
     {
-        //Creates the Menu object and instantiate the ArrayList of MenuItems
-        ArrayList<MenuItem> menuList = new ArrayList<>();
+        
     }
-    
+
     public void setMenuItem(String code, String name, double price)
-    {
-        MenuItem menuItem = new MenuItem(code, name, price);
+    {      
+        //menuList.add(new MenuItem(code, name, price));
     }
-    
+
     @Override
     public String toString()
     {
