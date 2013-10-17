804000ace6feeab4d3372d34a5d9dc7d01320b74
nvanmiddendorp <nvanmiddendorp@yahoo.com>
2013-10-16 19:42:16 -0500

    Update Restaurant.java

--- Restaurant.java
+++ Restaurant.java
@@ -3,19 +3,19 @@ package restauranttester;
 import java.util.Scanner;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 
-public class Restaurant extends Menu
-{    
+public class Restaurant
+{
+    private ArrayList<MenuItem> menuList = new ArrayList<>();
+    
     Restaurant()
     {
          //Creates ArrayList of Server Objects      
     }
     
     public void createMenu()
-    {
-        //Creates the object of Menu
-        Menu menu = new Menu();
-        
+    {        
         //Reads file and sends the code, name and price to setMenuItem()
         String code, name;
         double price;
@@ -44,16 +44,18 @@ public class Restaurant extends Menu
     
     public void displayMenu()
     {
-        //fix this output
+        System.out.println("Code \t\t Name \t\t Price");
+        
+        //Prints menu
         for(int i = 0; i < menuList.size(); i++)
         {
-            System.out.print(menuList.get(i).getItemName());    
+            System.out.printf("\n %2s %25s \t %4.2f", menuList.get(i).getItemCode(), menuList.get(i).getItemName(), menuList.get(i).getItemPrice());
         }
     }
     
     public void displayServerList()
     {
-        //Displays the list of Servers in the 
+
     }
     
     public void restaurantActivity()
