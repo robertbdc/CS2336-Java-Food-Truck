924c6cca9283732415daa75f47599acae6b93bfe
nvanmiddendorp <nvanmiddendorp@yahoo.com>
2013-10-20 13:51:09 -0500

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -1,21 +1,54 @@
-package restauranttester;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
 
-public class Menu
-{   
-    //Poor, unused, forgotten class.
-    public Menu()
+public class Menu 
+{
+    private static ArrayList<MenuItem> menuList = new ArrayList<>();
+    
+    //Constructor
+    Menu()
     {
+    
+    }
+    
+    public void createMenu()
+    {
+        //Reads file and sends the code, name and price to setMenuItem()        
+        String FILE_NAME = "C:\\Users\\Neil\\Documents\\CS2336-501\\Menu.txt";
+        Path path = Paths.get(FILE_NAME);
         
+        String code, name;
+        double price;
+        
+        try
+        {
+            Scanner sc = new Scanner(path);
+            
+            while(sc.hasNextLine())
+            {
+                code = sc.next();
+                name = sc.next();
+                price = sc.nextDouble();               
+                MenuItem food = new MenuItem(code, name, price);
+                menuList.add(food);
+            }
+        }
+        catch(Exception ex)
+        {
+            System.out.print(ex.getCause());
+        }        
     }
-
-    public void setMenuItem(String code, String name, double price)
-    {      
-
-    }
-
-    @Override
-    public String toString()
+       
+    public void displayMenu()
     {
-        return " ";
+        System.out.println("Code \t\t Name \t\t Price");
+        
+        //Prints menu
+        for(int i = 0; i < menuList.size(); i++)
+        {
+            System.out.printf("\n %2s %25s \t %4.2f", menuList.get(i).getItemCode(), menuList.get(i).getItemName(), menuList.get(i).getItemPrice());
+        }
     }
 }
