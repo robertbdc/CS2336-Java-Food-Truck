5bdfb668384dd9be2bfe4a5f8acbaa6801c948b4
nvanmiddendorp <nvanmiddendorp@yahoo.com>
2013-11-11 20:41:00 -0600

    Update Server.java
    Pasted the wrong code previously

--- Server.java
+++ Server.java
@@ -1,40 +1,86 @@
-public class Check extends Activity
+import java.util.ArrayList;
+
+public class Server
 {
-    private double totalCheck;
+    private String name;
+    private Table tableList [];
+    private static ArrayList<Table> tables = new ArrayList<>();
     
-    public Check(String label)
+    public Server()
     {
-        super.setName("Check");
-        super.setTable(label);
-    }
-    
-    public void setTotalCheck(MenuItem mi)
-    {
-        totalCheck += mi.getItemPrice();
-    }
-    
-    @Override
-    public String toString()
-    {
-        return "\nTable " + super.getTable() + " is checking out with a total of $" + totalCheck + ".";
-    }
-    
-    @Override
-    public double processActivity()
-    {
-        return 1.0;
-    }
-    
-    @Override
-    public String getMessage(char ch)
-    {
-        String message;
         
-        if(ch == 'A')
-            message = "Thank you for eating with us! \nTable " + super.getTable() + " status has now been changed to: Available";
-        else
-            message = "Not a valid status";
+    }
+    
+    public Server(String name, int [] tableArray)
+    {
+        this.name = name;
+        tableList = new Table [tableArray.length];
         
-        return message;
+        String tableString;
+        
+        for(int i = 0; i < tableArray.length; i++)
+        {          
+            tableString = "T" + tableArray[i];
+            Table t = new Table(tableString);
+            tableList[i] = t;
+            tables.add(t);
+        }
+    }
+    
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+    
+    public String getName()
+    {
+        return this.name;
+    }
+    
+    public String getServerStatus()
+    {     
+        String output = "";
+        for(int i = 0; i < tableList.length; i++)
+        {
+            output += "\t" + tableList[i].getLabel() + "(" + tableList[i].getStatus() + ")";
+        }
+        
+        return output;
+    }
+    
+    public String getTableLabel(String label)
+    {
+        String tableLabel = "Table not Found";
+        
+        for(int i = 0; i < tableList.length; i++)
+        {
+            if(label.equals(tableList[i].getLabel()))
+                tableLabel = label;
+        }
+        
+        return tableLabel;
+    }
+    
+    public char getTableStatus(String label)
+    {
+        char tableStatus = '0';
+        
+        for(int i = 0; i < tables.size(); i++)
+        {
+            if(label.equals(tables.get(i).getLabel()))
+                tableStatus = tables.get(i).getStatus();
+        }
+        
+        return tableStatus;
+    }
+    
+    public void setTabelStatus(char status, String label)
+    {
+        for(int i = 0; i < tables.size(); i++)
+        {
+            if(label.equals(tables.get(i).getLabel()))
+                tables.get(i).setStatus(status);
+        }
     }
 }
+
