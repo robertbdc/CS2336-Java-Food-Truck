acac90a2d00a02b51e6ed0f94c7b35bd2636fce5
nvanmiddendorp <nvanmiddendorp@yahoo.com>
2013-11-11 20:42:03 -0600

    Update Table.java
    Up to date 11/11/13

--- Table.java
+++ Table.java
@@ -1,12 +1,35 @@
-
-
 public class Table 
 {
     private String label;
-    private char   status;
+    private char   status = 'A';
     
-    public Table(String s)
+    public Table()
     {
         
+    }
+    
+    public Table(String label)
+    {
+        this.label = label;
+    }
+    
+    public void setLabel(String label)
+    {
+        this.label = label;
+    }
+    
+    public void setStatus(char status)
+    {
+        this.status = status;
+    }
+    
+    public String getLabel()
+    {
+        return this.label;
+    }
+    
+    public char getStatus()
+    {
+        return this.status;
     }
 }
