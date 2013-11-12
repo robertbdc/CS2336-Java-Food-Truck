9c8502acd9706f01a8f7927883da2622929fbdea
nvanmiddendorp <nvanmiddendorp@yahoo.com>
2013-11-11 20:37:19 -0600

    Update Restaurant.java
    Up to date 11/11/13

--- Restaurant.java
+++ Restaurant.java
@@ -1,11 +1,10 @@
-
-import java.io.IOException;
 import java.util.Scanner;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.io.*;
 
-public class Restaurant 
+public class Restaurant
 {
     private static ArrayList<Server> servers = new ArrayList<>();
     
@@ -27,7 +26,7 @@ public class Restaurant
             while(sc.hasNextLine())
             {
                 code = sc.next();
-                name = sc.next();
+                name = sc.next().replace("_", " ");
                 price = sc.nextDouble();               
                 m1.createMenuItem(code, name, price);
             }
@@ -42,7 +41,7 @@ public class Restaurant
         path = Paths.get(FILE_NAME);
         
         String serverName, temp;
-        int [] tables = new int [6];
+        int [] tables;
         
         try
         {
@@ -56,17 +55,17 @@ public class Restaurant
                 serverName = temp;
                 
                 //Read in and format our lines
-                temp = sc.nextLine().replace(",", " ");
+                temp = sc.nextLine().replace(" ", "");
+                temp = temp.replace((","), " ");
                 String [] parsed = temp.split(" ");
+      
+                tables = new int [parsed.length];
                 
                 //take formatted lines and make them into arrays
-                tables[0] = Integer.parseInt(parsed[1]);
-                tables[1] = Integer.parseInt(parsed[2]);
-                tables[2] = Integer.parseInt(parsed[3]);
-                tables[3] = Integer.parseInt(parsed[4]);
-                tables[4] = Integer.parseInt(parsed[5]);
-                tables[5] = Integer.parseInt(parsed[6]);
-                
+                for(int i = 0; i < parsed.length; i++)
+                {                    
+                    tables[i] = Integer.parseInt(parsed[i]);
+                }               
                 Server s1 = new Server(serverName, tables);
                 servers.add(s1);
             }
@@ -84,20 +83,95 @@ public class Restaurant
     }
     
     public void displayServerList()
-    {
+    {       
+        System.out.println("\nName\tTables");
         for(int i = 0; i < servers.size(); i++)
         {
-            System.out.println(servers.get(i).getName());    
+            System.out.println(servers.get(i).getName() + servers.get(i).getServerStatus());         
         }
     }
     
     public void restaurantActivity()
     {
+        String activity;
+        Scanner sc = new Scanner(System.in);
         
+        System.out.print("\nEnter activity:");     
+        activity = sc.nextLine();
+
+        processActivity(activity);
     }
     
-    public void processActivity()
-    {
+    public void processActivity(String activity)
+    {       
+        String [] element = activity.split("\\s");
         
+        for(int i = 0; i < servers.size(); i++)
+        {
+            try
+            {              
+            //if the activity is always "T##....." this checks to make sure the table is valid
+            if(element[0].equals(servers.get(i).getTableLabel(element[0])))
+            {              
+                //if the activity is always "T## P##" when seating guests, this checks for a P and creates the arrival stuff
+                if(element[1].charAt(0) == 'P')
+                {
+                    Arrival arrival = new Arrival(servers.get(i).getName(), servers.get(i).getTableLabel(element[0]), element[1].charAt(1));
+                    
+                    System.out.println(arrival.toString());
+                    servers.get(i).setTabelStatus('O', element[0]);
+                    System.out.println(arrival.getMessage('O'));
+                }
+                else if(element[1].charAt(0) == 'O')
+                {
+                    Menu m3 = new Menu();
+                    Order order = new Order(servers.get(i).getTableLabel(element[0]));
+                    
+                    for(int j = 2; j < element.length; j++)
+                    {
+                        order.addOrder(m3.getMenuItem(element[j]));
+                    }
+                    
+                    System.out.println(order.toString());
+                    servers.get(i).setTabelStatus('R', element[0]);
+                    System.out.println(order.getMessage('R'));
+                }
+                else if(element[1].charAt(0) == 'S')
+                {
+                    Service service = new Service(servers.get(i).getTableLabel(element[0]));
+                    
+                    System.out.println(service.toString());
+                    servers.get(i).setTabelStatus('S', element[0]);
+                    System.out.println(service.getMessage('S'));
+                }
+                else if(element[1].charAt(0) == 'C')
+                {
+                    Menu m4 = new Menu();
+                    Check check = new Check(servers.get(i).getTableLabel(element[0]));
+                    
+                    for(int k = 2; k < element.length; k++)
+                    {
+                        check.setTotalCheck(m4.getMenuItem(element[k]));
+                    }
+                    
+                    System.out.println(check.toString());
+                    servers.get(i).setTabelStatus('A', element[0]);
+                    System.out.println(check.getMessage('A'));
+                }
+                else
+                {
+                    System.out.print("\nYou did not enter a valid service code.  "
+                            + "Please try again using the format T## <service code> ... "
+                            + "\nService codes are: P (seat), O (order), S (serve), and C (check).");
+                }
+            }
+            }
+            catch(RuntimeException ex) //This isn't working yet
+            {
+                System.out.println(ex.toString());
+                
+            }
+        }
     }  
 }
+
